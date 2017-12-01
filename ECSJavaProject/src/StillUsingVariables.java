fb84104c141deb1c50348755baa2f15dd46cc1a4
Lucwells <lucwells7@gmail.com>
2017-12-01 10:55:45 -0800

    Assignment 2

---
+++ ECSJavaProject/src/StillUsingVariables.java
@@ -0,0 +1,18 @@
+package VariablesAssignment;
+
+public class StillUsingVariables
+{
+	public static void main(String [] args)
+	{
+		int mygradyear;
+		String myName;
+		
+		mygradyear=2021;
+		myName="Luc Wells";
+		
+		System.out.println("My name is " +myName+ " and I'll graduate in " +mygradyear+ ".");
+		
+	}
+	
+
+}
